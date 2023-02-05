925cf4831f65f5b0845792cee0b1df356c67d1bc
IMRANDIL <aliimranadil2@gmail.com>
2023-02-05 14:48:56 +0530

    some more go added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func(app *application) routes() http.Handler{
+//create a router mux
+mux := chi.NewRouter()
+
+
+return mux
+
+}
