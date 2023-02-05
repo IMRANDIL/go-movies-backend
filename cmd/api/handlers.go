23ffdffbe2b59921fdd671afb69b1c490368c851
IMRANDIL <aliimranadil2@gmail.com>
2023-02-05 16:03:33 +0530

    some more added

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -42,7 +42,25 @@ rd,_ := time.Parse("2006-02-12","1986-03-07")
 		MPAARating: "R",
 		RunTime: 116,
 		Description: "A very nice movie",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
 
 
 	}
+	movies = append(movies, highlander)
+	rd,_ = time.Parse("2006-02-12","2010-03-07")
+	hateStory := models.Movie{
+		ID: 1,
+		Title: "Hate Story",
+		ReleaseDate: rd,
+		MPAARating: "AA",
+		RunTime: 120,
+		Description: "A very nice hate story movie",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+
+
+	}
+
+	movies = append(movies, hateStory)
 }
