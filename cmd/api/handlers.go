a6b34ff96c9b486417f45af864ae39db53e260fb
IMRANDIL <aliimranadil2@gmail.com>
2023-02-05 15:33:03 +0530

    some more go change

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) Home (w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "hello world from %s",app.Domain)
+	var payload = struct {
+		Status string `json: "status"`
+		Message string `json: "message"`
+		Version string `json: "version"`
+	}{
+		Status: "active",
+		Message: "Go movies up and running",
+		Version: "1.0.0",
+	}
+
+	
 }
