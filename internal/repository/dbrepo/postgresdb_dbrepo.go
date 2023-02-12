1e00a1779f014df716975eecc4374852d47436e2
IMRANDIL <aliimranadil2@gmail.com>
2023-02-12 17:31:26 +0530

    some more go added

--- internal/repository/dbrepo/postgresdb_dbrepo.go
+++ internal/repository/dbrepo/postgresdb_dbrepo.go
@@ -2,14 +2,21 @@ package dbrepo
 
 import (
 	"backend/internal/models"
+	"context"
 	"database/sql"
+	"time"
 )
 
 type DatabaseRepo struct {
 	DB *sql.DB
 }
 
+const dbTimeout = time.Second * 3
+
+
 func (m *DatabaseRepo) AllMovies() ([]*models.Movie, error) {
+	ctx, cancel := context.WithTimeout(context.Background(),dbTimeout)
+	defer cancel()
 	var movies []*models.Movie
 
 	return movies, nil
