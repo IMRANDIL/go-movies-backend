ec3a12965f0e21c9e4af1c7b3a4dc42374ce72fe
IMRANDIL <aliimranadil2@gmail.com>
2023-02-12 17:25:02 +0530

    some more change

--- internal/repository/dbrepo/postgresdb_dbrepo.go
+++ internal/repository/dbrepo/postgresdb_dbrepo.go
@@ -1,7 +1,16 @@
 package dbrepo
 
-import "database/sql"
+import (
+	"backend/internal/models"
+	"database/sql"
+)
 
 type DatabaseRepo struct {
 	DB *sql.DB
+}
+
+func (m *DatabaseRepo) AllMovies() ([]*models.Movie, error) {
+	var movies []*models.Movie
+
+	return movies, nil
 }
