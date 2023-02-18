617a9443f0e504d825c56cbf5a049c824e32690b
IMRANDIL <aliimranadil2@gmail.com>
2023-02-18 17:04:27 +0530

    go go go

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -32,4 +33,42 @@ type TokenPairs struct {
 
 type Claims struct {
 	jwt.RegisteredClaims
+}
+
+
+func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
+	// create a token
+
+	token := jwt.New(jwt.SigningMethodES256)
+
+
+	// Set the claims
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = j.Audience
+	claims["iss"] = j.Issuer
+	claims["iat"] = time.Now().UTC().Unix()
+	claims["typ"] = "JWT"
+
+	// Set the expiry for JWT
+
+	// Create a signed token
+
+	// create a refresh token and set claims
+
+	// set the expiry for the refresh token
+
+	// create signed refresh token...
+	
+	// create tokenPairs and populate with signed tokens
+
+	// return tokenpairs
+
+
+
+
+
 }
